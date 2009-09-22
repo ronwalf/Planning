ac5b3d4f91aff057c04a98f5cf3edb23706fc8c8
ronwalf <ronwalf@3bc8eda9-e039-0410-a36e-9c2a3ed70af7>
2009-09-22 20:36:16 +0000

    missing source file (pddlcheck)
    git-svn-id: https://subversion.umiacs.umd.edu/dsngroup/ronwalf/2008/haskell/Planning@1630 3bc8eda9-e039-0410-a36e-9c2a3ed70af7

---
+++ pddlcheck.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE OverlappingInstances#-}
+module Main where
+
+import System.Environment
+import System.IO
+import Text.ParserCombinators.Parsec (runParser)
+
+import Planning.PDDL.PDDL3_0
+
+main :: IO ()
+main = do
+    args <- getArgs
+    hPutStrLn stderr $ "Parsing " ++ show args
+    mapM_ (\f -> do
+        ftxt <- readFile f
+        let res = runParser pddlDomainParser emptyDomain f ftxt
+        printResults f res) args
+    where
+        printResults f (Left err) = do
+            putStrLn $ "Error in file " ++ f
+            print err
+        printResults f (Right domain) = 
+            putStrLn $ f ++ " OK"
